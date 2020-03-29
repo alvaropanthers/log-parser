a3b8da1f38361e0c61165cab87155d2db2ce4767
Alvaro Orozco <aoroz032@fiu.edu>
2020-03-29 16:36:56 -0400

    Refactoring

--- logparser/authlog.py
+++ logparser/authlog.py
@@ -1,7 +1,15 @@
+from datetime import datetime
+
+_MONTHNUMBER = {'Jan': 1, 'Feb': 2, 'Mar': 3, 'Apr': 4, 'May': 5, 'Jun': 6, 'Jul': 7, 'Agu': 8, 'Sep': 9, 'Oct': 10, 'Nov': 11, 'Dec': 12}
+_MONTHNAMES = [None, "Jan", "Feb", "Mar", "Apr", "May", "Jun",
+                     "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
 class Log:
     def __init__(self, user, ip):
         self.users = [user]
         self.ip = ip
+
+    def addUser(self, user):
+        self.users.append(user)
 
     def print(self):
         print(f"ip: {self.ip};")
@@ -14,34 +22,28 @@ class Log:
         return f"user: {self.users[0].user} ip: {self.ip} port: {self.users[0].port}"
 
 class User:
-    def __init__(self, user, port, date, validuser):
+    def __init__(self, user, port, datetime, validuser):
         self.user = user
         self.port = port
-        self.date = date
+        self.datetime = datetime
         self.validuser = validuser
 
     def print(self):
         return f"user: {self.user} port: {self.port}"
 
     def __str__(self):
-        return f"(Invalid user: {str(self.validuser)}), {self.date.month} {self.date.day} {self.date.time}; {self.user}, (port: {self.port})"
-
-class Date:
-    def __init__(self, month, day, time):
-        self.month = month
-        self.day = day
-        self.time = time
+        return f"(Valid user: {str(self.validuser)}), {self.datetime.ctime()} ; {self.user}, port: {self.port}"
 
 class AuthLog:
     def __init__(self, filepath):
         self.filepath = filepath
-        self.FAILED_LOC = 5
-        self.IP_LOC = 12
-        self.USERNAME_LOC = 10
-        self.PORT_LOC = 14
-        self.INVALID_LOC = 8
-        self.FOR_LOC = 7
-        self.VALID_USER_ADJUST = 2
+        self._FAILED_LOC = 5
+        self._IP_LOC = 12
+        self._USERNAME_LOC = 10
+        self._PORT_LOC = 14
+        self._INVALID_LOC = 8
+        self._FOR_LOC = 7
+        self._VALID_USER_ADJUST = 2
 
     def print_log(self):
         records = self.parse_file()
@@ -58,35 +60,29 @@ class AuthLog:
         with open(self.filepath, 'r') as file:
             records = []
             for line in file:
-                retval = self.call_back(line.strip().split(' '), records)
-                if retval:
-                    records.append(retval)
+                record = self.createRecord(line.strip().split(' '), records)
+                if record:
+                    records.append(record)
 
         return records
 
-    def call_back(self, line, data):
-        lineLen = len(line)
-        if (0 <= lineLen < 14) or line[self.FAILED_LOC] != 'Failed':
+    def createRecord(self, line, data):
+        if (0 <= len(line) < 14) or line[self._FAILED_LOC] != 'Failed':
             return None
 
-        ip = username = port =  ''
-        validuser = False
+        invalid = line[self._INVALID_LOC] == 'invalid'
+        adjust = 0 if invalid else self._VALID_USER_ADJUST
+        ip = line[self._IP_LOC - adjust]
+        username = line[self._USERNAME_LOC - adjust]
+        port = line[self._PORT_LOC - adjust]
 
-        if line[self.INVALID_LOC] == 'invalid':
-            ip = line[self.IP_LOC]
-            username = line[self.USERNAME_LOC]
-            port = line[self.PORT_LOC]
-            validuser = False 
-        else:  # line[FOR_LOC] == 'for'
-            ip = line[self.IP_LOC - self.VALID_USER_ADJUST]
-            username = line[self.USERNAME_LOC - self.VALID_USER_ADJUST]
-            port = line[self.PORT_LOC - self.VALID_USER_ADJUST]
-            validuser = True 
+        time = [ int(item.lstrip('0')) for item in line[2].split(':') ]
+        today = datetime.now().replace(month=_MONTHNUMBER[line[0]], day=int(line[1].lstrip('0')), hour=time[0], minute=time[1], second=time[2])
+        user = User(username, port, today, not invalid)
                 
         for i in range(len(data)):
             if data[i].ip == ip:
-                data[i].users.append(User(username, port, Date(month = line[0], day = line[1], time = line[2]), validuser))
+                data[i].addUser(user)
                 return None
 
-        return Log(User(username, port, Date(month = line[0], day = line[1], time = line[2]), validuser), ip)
-
+        return Log(user, ip)
